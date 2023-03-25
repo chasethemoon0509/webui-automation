37a31ec2ecf497e4839fb0f0a6bbc52aabdde24e
chris <2762851151@qq.com>
2023-03-25 23:11:36 +0800

    测试用例

--- test_case/test_demo.py
+++ test_case/test_demo.py
@@ -3,7 +3,7 @@ import pytest
 from page_object.demo_page import DemoPage
 from selenium import webdriver
 from utils.log import log
-from page_locator.demo_page_locator import login_result_ele
+from locator_object.demo_page_locator import login_result_ele
 
 
 @pytest.mark.usefixtures('get_driver')
@@ -14,3 +14,13 @@ class TestDemo:
         DemoPage(get_driver).login(account, password)
         DemoPage(get_driver).ast_eq(DemoPage(get_driver).find(login_result_ele, timeout=5).text, '登录结果：成功', login_result_ele)
 
+    def test_2(self):
+        assert 1 + 1 == 2
+
+    @pytest.mark.xfail
+    def test_3(self):
+        assert 1 + 1 == 3
+
+    @pytest.mark.skip
+    def test_4(self):
+        log.info('用例 test_4 跳过不执行')
